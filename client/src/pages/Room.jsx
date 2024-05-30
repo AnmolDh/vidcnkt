434e48b26dee5b2c8ce97c296e8abb4465fc8b6d
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2024-05-30 16:25:02 +0530

    self stream

--- client/src/pages/Room.jsx
+++ client/src/pages/Room.jsx
@@ -1,11 +1,20 @@
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useSocket } from "../context/SocketProvider"
+import ReactPlayer from "react-player";
 
 export default function Room() {
   const socket = useSocket();
+  const [remoteSocketId, setRemoteSocketId] = useState(null);
+  const [myStream, setMyStream] = useState();
 
   const handleUserJoined = useCallback(({ email, id }) => {
     console.log(`email ${email} joined`);
+    setRemoteSocketId(id);
+  }, [])
+
+  const handleCallUser = useCallback(async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+    setMyStream(stream);
   }, [])
 
   useEffect(() => {
@@ -15,5 +24,11 @@ export default function Room() {
 
   return <>
     Room Page
+    <h4>{remoteSocketId ? "connected" : "not connected"}</h4>
+    {remoteSocketId && <button onClick={handleCallUser}>Call</button>}
+    <>
+      <h3>My Stream</h3>
+      {myStream && <ReactPlayer playing muted url={ myStream } />}
+    </>
   </>
 }
