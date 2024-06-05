89d0f6de994ddd735dba360b4836aabf9c16d467
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2024-06-05 20:49:16 +0530

    init express

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
+const express = require("express");
 const { Server } = require("socket.io");
 const socketHandlers = require("./handlers/socketHandlers");
 
-const io = new Server(8000, {
+const app = express();
+const server = require("http").createServer(app);
+const io = new Server(server, {
   cors: true
 });
 
@@ -12,3 +15,9 @@ io.on("connection", socket => {
   console.log("socket connected: ", socket.id);
   socketHandlers(io, socket, emailToSocketId, socketIdToEmail);
 })
+
+app.get("/", (req, res) => {
+  res.send("hello");
+})
+
+server.listen(8000, () => console.log("server running!!"));
