a224a8e729853185a298b6c840c52a26ee05e81f
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2024-06-05 20:36:49 +0530

    server socketHandlers

---
+++ server/handlers/socketHandlers.js
@@ -0,0 +1,26 @@
+module.exports = function (io, socket, emailToSocketId, socketIdToEmail) {
+  socket.on("room:join", (data) => {
+    const { email, room } = data;
+    emailToSocketId.set(email, socket.id);
+    socketIdToEmail.set(socket.id, email);
+    io.to(room).emit("user:joined", { email, id: socket.id });
+    socket.join(room);
+    io.to(socket.id).emit("room:join", data);
+  });
+
+  socket.on("user:call", ({ to, offer }) => {
+    io.to(to).emit("incoming:call", { from: socket.id, offer });
+  });
+
+  socket.on("call:accepted", ({ to, ans }) => {
+    io.to(to).emit("call:accepted", { from: socket.id, ans });
+  });
+
+  socket.on("peer:nego:needed", ({ to, offer }) => {
+    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+  });
+
+  socket.on("peer:nego:done", ({ to, ans }) => {
+    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+  });
+};
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const { Server } = require("socket.io");
+const socketHandlers = require("./handlers/socketHandlers");
 
 const io = new Server(8000, {
   cors: true
@@ -9,29 +10,5 @@ const socketIdToEmail = new Map();
 
 io.on("connection", socket => {
   console.log("socket connected: ", socket.id);
-
-  socket.on("room:join", (data) => {
-    const { email, room } = data;
-    emailToSocketId.set(email, socket.id);
-    socketIdToEmail.set(socket.id, email);
-    io.to(room).emit("user:joined", { email, id: socket.id });
-    socket.join(room);
-    io.to(socket.id).emit("room:join", data);
-  });
-
-  socket.on("user:call", ({ to, offer }) => {
-    io.to(to).emit("incoming:call", {from: socket.id, offer})
-  })
-
-  socket.on("call:accepted", ({ to, ans }) => {
-    io.to(to).emit("call:accepted", { from: socket.id, ans });
-  })
-
-  socket.on("peer:nego:needed", ({ to, offer }) => {
-    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
-  })
-
-  socket.on("peer:nego:done", ({ to, ans }) => {
-    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
-  })
+  socketHandlers(io, socket, emailToSocketId, socketIdToEmail);
 })
