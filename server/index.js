eb97356f3d0b64a5ca19be5a8a17d4da2a97822b
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2024-06-06 21:54:11 +0530

    auth logic base

--- server/index.js
+++ server/index.js
@@ -2,7 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { Server } = require("socket.io");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const socketHandlers = require("./handlers/socketHandlers");
+const { User } = require("./models/models");
 require("dotenv").config();
 
 const app = express();
@@ -11,7 +14,7 @@ app.use(bodyParser.json());
 
 const server = require("http").createServer(app);
 const io = new Server(server, {
-  cors: true
+  cors: true,
 });
 
 mongoose.connect(process.env.MONGODB_URL);
@@ -19,18 +22,39 @@ mongoose.connect(process.env.MONGODB_URL);
 const emailToSocketId = new Map();
 const socketIdToEmail = new Map();
 
-io.on("connection", socket => {
+io.on("connection", (socket) => {
   console.log("socket connected: ", socket.id);
   socketHandlers(io, socket, emailToSocketId, socketIdToEmail);
-})
+});
 
 app.get("/", (req, res) => {
   res.send("hello");
-})
+});
 
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
-  console.log(name, email, password);
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = new User({ name, email, password:hashedPassword });
+  await user.save();
+  const token = jwt.sign({ userId: email }, process.env.JWT_SECRET);
+  res.json({ success: true, token });
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  
+  if (!user) {
+    return res.status(404).json({ success: false, error: "user not found" });
+  }
+
+  const passwordMatched = await bcrypt.compare(password, user.password);
+  if (!passwordMatched) {
+    return res.status(400).json({ success: false, error: "invalid password" });
+  }
+
+  const token = jwt.sign({ userId: email }, process.env.JWT_SECRET);
+  res.json({ success: true, token });
 })
 
 server.listen(process.env.PORT, () => console.log("server running!!"));
