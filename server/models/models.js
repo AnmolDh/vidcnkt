e6b36c33d600e9b1520df13b63ef941924114837
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2024-06-06 09:56:43 +0530

    user model

---
+++ server/models/models.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    require: true
+  }
+})
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User };
