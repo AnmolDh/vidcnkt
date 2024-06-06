0dc0b6a8f4aab4d7e0fdeca0fe3f89306c7300d0
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2024-06-06 22:03:13 +0530

    routes handler

--- server/index.js
+++ server/index.js
@@ -2,15 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { Server } = require("socket.io");
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 const socketHandlers = require("./handlers/socketHandlers");
-const { User } = require("./models/models");
+const handleRoutes = require("./routes/routesHandler");
 require("dotenv").config();
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(handleRoutes);
 
 const server = require("http").createServer(app);
 const io = new Server(server, {
@@ -26,35 +25,5 @@ io.on("connection", (socket) => {
   console.log("socket connected: ", socket.id);
   socketHandlers(io, socket, emailToSocketId, socketIdToEmail);
 });
-
-app.get("/", (req, res) => {
-  res.send("hello");
-});
-
-app.post("/register", async (req, res) => {
-  const { name, email, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const user = new User({ name, email, password:hashedPassword });
-  await user.save();
-  const token = jwt.sign({ userId: email }, process.env.JWT_SECRET);
-  res.json({ success: true, token });
-});
-
-app.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
-  
-  if (!user) {
-    return res.status(404).json({ success: false, error: "user not found" });
-  }
-
-  const passwordMatched = await bcrypt.compare(password, user.password);
-  if (!passwordMatched) {
-    return res.status(400).json({ success: false, error: "invalid password" });
-  }
-
-  const token = jwt.sign({ userId: email }, process.env.JWT_SECRET);
-  res.json({ success: true, token });
-})
 
 server.listen(process.env.PORT, () => console.log("server running!!"));
---
+++ server/routes/routesHandler.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { User } = require("../models/models");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = new User({ name, email, password: hashedPassword });
+  await user.save();
+  const token = jwt.sign({ userId: email }, process.env.JWT_SECRET);
+  res.json({ success: true, token });
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(404).json({ success: false, error: "user not found" });
+  }
+
+  const passwordMatched = await bcrypt.compare(password, user.password);
+  if (!passwordMatched) {
+    return res.status(400).json({ success: false, error: "invalid password" });
+  }
+
+  const token = jwt.sign({ userId: email }, process.env.JWT_SECRET);
+  res.json({ success: true, token });
+});
+
+module.exports = app;
