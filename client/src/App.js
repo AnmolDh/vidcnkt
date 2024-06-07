4145450f37febbef2f688cc2e81b73b167d2cc18
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2024-06-07 21:10:08 +0530

    basic login UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Lobby from "./pages/Lobby";
 import Room from "./pages/Room";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Routes>
+      <Route path="/login" element={<Login />} />
       <Route path="/" element={<Lobby />} />
-      <Route path="/room/:roomId" element={<Room/>}/>
+      <Route path="/room/:roomId" element={<Room />} />
     </Routes>
   );
 }
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import axios from "axios";
+
+export default function Login() {
+  const [loginDetails, setLoginDetails] = useState({
+    email: "",
+    password: ""
+  })
+
+  const handleInput = (e) => {
+    setLoginDetails({
+      ...loginDetails,
+      [e.target.id]: e.target.value
+    })
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await axios.post(`${process.env.REACT_APP_SERVER_URL}/login`, loginDetails);
+    console.log(res);
+  }
+
+  return <>
+    <form>
+      <label htmlFor="email">Email:</label>
+      <input type="email" id="email" onChange={handleInput}></input>
+
+      <label htmlFor="password">Password:</label>
+      <input type="password" id="password" onChange={handleInput}></input>
+
+      <button type="submit" onClick={handleSubmit}>login</button>
+    </form>
+  </>
+}
